c1ad989436133633d4f5a1665d88ddcded0b64cf
krish3402 <mk.tala654@gmail.com>
2019-09-21 19:12:21 +0530

    chek in

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace BuildApp
         [STAThread]
         static void Main()
         {
-           
+           //*Mohan Krishna*//
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
